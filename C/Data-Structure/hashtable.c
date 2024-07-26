9c07dc6e17599e4b5159b6b53f8788e0ebbe4dec
Arthur_Negrão <arthurnegrao10s@gmail.com>
2024-07-26 12:06:48 -0300

    Adicionada a pasta de estrutura de dados e linked list e hashtable

---
+++ C/Data-Structure/hashtable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define array_size 10
+
+
+typedef struct Hashtable
+{
+    int *key;
+    int *item;
+
+} Hashtable;
+
+typedef struct HashtableItem
+{
+    int hash;
+    char name[100];
+    int *next;
+    int *previous;
+
+} HashtableItem;
+
+
+int main() 
+{
+    return 0;
+}
