25a554ce1b8f3bd890ade860fea1988569e3dc3b
Arthur_Negrão <arthurnegrao10s@gmail.com>
2024-12-27 22:56:56 -0300

    Criada a função para limpar o buffer do teclado e evitar erros na leitura dos dados

--- C/Data-Structure/Heterogeneous_Linkedlist/main.c
+++ C/Data-Structure/Heterogeneous_Linkedlist/main.c
@@ -2,8 +2,10 @@
 
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
-void clear(void);
+
+void clean_stdin();
 void input(int*, char*);
 void printMenu(int*);
 void typesMenu(linkedlist*);
@@ -17,6 +19,8 @@ int main() {
     int *user = (int*) malloc(sizeof(int*));
     *user = 0;
 
+    short unsigned int await = 0;
+
 
     while (*user != 6) {
         printMenu(user);
@@ -27,16 +31,23 @@ int main() {
             break;
 
         case 2:
+            int *index = (int*) malloc(sizeof(int*));
+            input(index, "Digite o índice do nó que deseja apagar: ");
+            pop(list, *index, false);
             break;
         
         case 3:
             break;
 
         case 4:
+            printf("A lista possui %d nós\n", list->length);
             printList(list);
             break;
 
         case 5:
+            deleteList(list);
+            linkedlist *list = createLinkedlist();
+            printf("A lista foi apagada com sucesso\n");
             break;
 
         case 6:
@@ -49,10 +60,15 @@ int main() {
     return 0;
 }
 
-// Function to clear the terminal
-void clear(void) {
-    printf("\e[1;1H\e[2J");
+
+// Function to clean the buffer
+void clean_stdin() {
+    char c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
 }
+
 
 /*
 Collect input from the user keyboard
@@ -63,7 +79,7 @@ text: (char*)  String with text to print on screen
 void input(int *user, char *text) {
     printf("%s", text);
     scanf("%d", user);
-    fflush(stdin);
+    clean_stdin();
 }
 
 
@@ -73,8 +89,8 @@ Print the main menu
 user: (int*) Pointer to user input
 */
 void printMenu(int *user) {
-    clear();
-    puts("Menu de opções da lista heterogênea");
+    // clear();
+    puts("\nMenu de opções da lista heterogênea");
     puts("1. Adicionar um valor");
     puts("2. Remover um valor");
     puts("3. Procurar um nó");
@@ -92,8 +108,8 @@ list: (linkedlist*) List with all data storage
 */
 void typesMenu(linkedlist *list) {
 
-    clear();
-    puts("Tipos de dados:");
+    // clear();
+    puts("\nTipos de dados:");
     puts("0. int");
     puts("1. float");
     puts("2. double");
@@ -102,7 +118,8 @@ void typesMenu(linkedlist *list) {
     puts("5. void");
     int *data_type = (int*) malloc(sizeof(int*));
     input(data_type, "Qual o tipo de dado que deseja adicionar? ");
-    printf("Digite o valor do dado selecionado: ");
+    if (*data_type < VOID)
+        printf("Digite o valor do dado selecionado: ");
 
     TypedData data;
     short unsigned int type;
@@ -111,31 +128,32 @@ void typesMenu(linkedlist *list) {
     {
     case INT: 
         scanf("%lld", &data.intNumber);
-        fflush(stdin);
+        clean_stdin();
         type = INT;
         break;
 
     case FLOAT:
         scanf("%f", &data.floatNumber);
-        fflush(stdin);
+        clean_stdin();
         type = FLOAT;
         break;
     
     case DOUBLE:
         scanf("%lf", &data.doubleNumber);
-        fflush(stdin);
+        clean_stdin();
         type = DOUBLE;
         break;
 
     case STRING:
         fgets(data.string, sizeof(data.string), stdin);
-        fflush(stdin);
+        data.string[strcspn(data.string, "\n")] = '\0';
+        clean_stdin();
         type = STRING;
         break;
 
     case BOOL:
         scanf("%d", &data.boolean);
-        fflush(stdin);
+        clean_stdin();
         type = BOOL;
         break;
 
@@ -153,7 +171,7 @@ void typesMenu(linkedlist *list) {
     int pos = 0;
     printf("Qual a posição que deseja adicionar? ");
     scanf("%d", &pos);
-    fflush(stdin);
+    clean_stdin();
 
     hNode *node = createNode(data, type);
     insert(list, node, pos);
